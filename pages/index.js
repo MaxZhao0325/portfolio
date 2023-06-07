ce5ac45f39f01568f4ad253f8abe2b6361540979
Hanzhang Zhao <hz9xs@virginia.edu>
2023-06-07 17:07:57 +0800

    update my personal description

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[I am Hanzhang, a fourth year student at UVA]</p>
+        <p>[I am Hanzhang, a fourth year student at UVA. I am majoring in Computer Science and Mathematics, and I want to be a software engineer in the future.]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
